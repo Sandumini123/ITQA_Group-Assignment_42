9be93549ccff70b390f43dae82d8046983a05885
Jayanithyaa <jayanithyamadhushani@icloud.com>
2024-12-31 15:35:26 +0530

    API folder structure initial part

---
+++ API_Testing/playwright.config.js
@@ -0,0 +1,37 @@
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  
+  reporter: 'html',
+
+  use: { 
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://127.0.0.1:3000',
+
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+
+    
+  ],
+
+});
+
